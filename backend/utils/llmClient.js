a7a3e5d4dcc173adf9775d5fa76d2d4cf51a6e14
Xuechun Hua <122423248+XuechunHHH@users.noreply.github.com>
2024-11-29 19:24:40 -0800

    change deployment params

--- backend/utils/llmClient.js
+++ backend/utils/llmClient.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const google_llm_api_key = process.env.GOOGLE_LLM_API_KEY;
+const google_llm_api_key = process.env.GOOGLE_LLM_API_KEY || 'AIzaSyDxAfnwI78Lf7UslP-16_uK_CmUKm88BFg';
 
 const genAI = new GoogleGenerativeAI(google_llm_api_key);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
